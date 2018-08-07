5214b4cea3bc801fa1125e1fe3880c3c9b433702
Jan Hořava <czechinthemail@gmail.com>
2018-08-07 15:25:10 -0700

    added some command buttons

--- gmod-iterami-addon/lua/autorun/gmod-iterami-addon.lua
+++ gmod-iterami-addon/lua/autorun/gmod-iterami-addon.lua
@@ -6,13 +6,112 @@ hook.Add(
   function()
       spawnmenu.AddToolMenuOption(
         "iterami",
-        "Settings",
-        "GMod-iterami-Addon",
-        "GMod-iterami-Addon",
+        "Commands",
+        "GMod",
+        "GMod",
         "",
         "",
         function(panel)
             panel:ClearControls()
+
+            local commands = {
+              'clear',
+              'disconnect',
+              'flush',
+              'quit',
+            }
+            local y = 0;
+            for i, command in ipairs(commands) do
+                y = y + 23
+
+                local button = vgui.Create(
+                  "DButton",
+                  panel
+                )
+                button:SetPos(
+                  0,
+                  y
+                )
+                button:SetText(command)
+                button.DoClick = function()
+                    RunConsoleCommand(
+                      command,
+                      ""
+                    )
+                end
+            end
+        end
+      )
+      spawnmenu.AddToolMenuOption(
+        "iterami",
+        "Commands",
+        "Player",
+        "Player",
+        "",
+        "",
+        function(panel)
+            panel:ClearControls()
+
+            local commands = {
+              'god',
+              'kill',
+              'noclip',
+            }
+            local y = 0;
+            for i, command in ipairs(commands) do
+                y = y + 23
+
+                local button = vgui.Create(
+                  "DButton",
+                  panel
+                )
+                button:SetPos(
+                  0,
+                  y
+                )
+                button:SetText(command)
+                button.DoClick = function()
+                    RunConsoleCommand(
+                      command,
+                      ""
+                    )
+                end
+            end
+        end
+      )
+      spawnmenu.AddToolMenuOption(
+        "iterami",
+        "Commands",
+        "World",
+        "World",
+        "",
+        "",
+        function(panel)
+            panel:ClearControls()
+
+            local commands = {
+              'r_cleardecals',
+            }
+            local y = 0;
+            for i, command in ipairs(commands) do
+                y = y + 23
+
+                local button = vgui.Create(
+                  "DButton",
+                  panel
+                )
+                button:SetPos(
+                  0,
+                  y
+                )
+                button:SetText(command)
+                button.DoClick = function()
+                    RunConsoleCommand(
+                      command,
+                      ""
+                    )
+                end
+            end
         end
       )
   end
